5ba70c8eca5d72e87a0b8f2aae820560981dd03a
Atul rathi <rathiatul43@gmail.com>
2025-08-30 11:43:16 +0530

    add login and sign up page with ejs

--- app.js
+++ app.js
@@ -5,20 +5,26 @@ const db=require('./config/Mongoose-connection');
 const ownerRouter=require('./router/ownerRouter');
 const userRouter=require('./router/userRouter');
 const productsRouter=require('./router/productsRouter');
+const index=require('./router/index');
 
 const cookieParser = require('cookie-parser');
 const path=require('path');
 
-app.use(express.json);
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser);
-app.use(express.static(path.join(__dirname,'public')));
-app.set('view engine','ejs');
+//middleware
+  app.use(express.json());
+  app.use(express.urlencoded({extended:true}));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname,'public')));
+  app.set('view engine','ejs');
+  app.set('views',path.join(__dirname,'views'));
 
+//routes
+app.use('/',index);
 app.use('/owner',ownerRouter);
 app.use('/user',userRouter);
 app.use('/product',productsRouter);
 
+//port
 app.listen(3000,()=>{
   console.log('running')
 });
--- config/Mongoose-connection.js
+++ config/Mongoose-connection.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const config=require('config');
+const dbgr=require('debug')('development:mongoose'); 
 
-mongoose.connect('mongodb://127.0.0.1:27017/scatch')
+mongoose.connect(`mongodb://127.0.0.1:27017/scatch`)
 .then(function(){
   console.log('connected')
 })
 .catch((err)=>{
-  console.col(err)
+  console.log(err)
 });
 
-module.exports=mongoose.connection
+module.exports=mongoose.connection;
